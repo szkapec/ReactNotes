d662982cf1bce199702c2d6a290209c27c9bbdd4
szkapec <szkapec999@o2.pl>
2020-02-17 12:40:25 +0100

    Updates

---
+++ precache-manifest.d82146a9c535bdc75955a349d0807048.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "0725bbe36483800f0588",
+    "url": "/ReactNotes/static/js/main.2bf77759.chunk.js"
+  },
+  {
+    "revision": "2ce2cbc652e52c93f1d3",
+    "url": "/ReactNotes/static/js/runtime~main.3f292b48.js"
+  },
+  {
+    "revision": "6d52623685fd8fc88e73",
+    "url": "/ReactNotes/static/js/2.cdf653de.chunk.js"
+  },
+  {
+    "revision": "93d4f3ba834e7afdf3f8dcb93f2a82f8",
+    "url": "/ReactNotes/static/media/logo.93d4f3ba.svg"
+  },
+  {
+    "revision": "6fc1d8fe5a3f17e73f1ecd0d757475d1",
+    "url": "/ReactNotes/static/media/link.6fc1d8fe.svg"
+  },
+  {
+    "revision": "af737b5505b4cdb4a433cc3ac1f234af",
+    "url": "/ReactNotes/static/media/bulb.af737b55.svg"
+  },
+  {
+    "revision": "0969c92b498df082b2f18fc455b2fa13",
+    "url": "/ReactNotes/static/media/logout.0969c92b.svg"
+  },
+  {
+    "revision": "84b70f8a7155a0edb0576db0efc9ace2",
+    "url": "/ReactNotes/static/media/pen.84b70f8a.svg"
+  },
+  {
+    "revision": "051af8ba4d1984fc498ff6da805311cb",
+    "url": "/ReactNotes/static/media/twitter.051af8ba.svg"
+  },
+  {
+    "revision": "a14461b86078261a696a425d13ffe612",
+    "url": "/ReactNotes/static/media/magnifier.a14461b8.svg"
+  },
+  {
+    "revision": "e43ce6c85c8383cb4442972a422cf804",
+    "url": "/ReactNotes/static/media/plus.e43ce6c8.svg"
+  },
+  {
+    "revision": "d2c9da29f13473152ad956c5646b79f3",
+    "url": "/ReactNotes/index.html"
+  }
+];
